bdef4b88992ca703a9eea7fb41b059f39e0b0fca
Jeremy Landi <Jeremy Landi>
2016-04-18 09:14:08 -0700

    added ShaTar species and updated ISpace interface

--- GalaxyWars/GalaxyWars/ShaTar.cs
+++ GalaxyWars/GalaxyWars/ShaTar.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace GalaxyWars
 {
-  class ShaTar
+  class ShaTar : ISpace
   {
-    public string description = "The Sha'tar, or 'born of light,' are naaru that aided the Aldor, the order of draenei priests formerly led by Velen, in rebuilding Shattrath City. The city was destroyed by the Orcs during their rampage across Draenor prior to the First War. The Aldor and the Scryers fight for the favor of the Sha'tar so that they may be assisted in their war by the naaru's powers. The entity that leads the Sha'tar is known as A'dal; he can be found upon the Terrace of Light in Shattrath City.";
-    public string skin = "Sky Blue";
-    public string clothing = "advanced space suite";
+    public string description = "The Sha'tar, or 'born of light,' are Draenei that aided the creation of the New Space Order.";
+    public string skin = "Blue";
+    public string clothing = "Advanced Space Suite";
+    public string bodyType = "Short, Weak, Skinny";
     public bool bigLungs = true;
     public int horns = 2;
-    public bool advancedSpaceStation = true;
-   
+    public int startingPopualtion = 1000000;
+
     private Spaceship _spaceship;
 
     public Spaceship spaceship
@@ -27,6 +28,21 @@ namespace GalaxyWars
       {
         _spaceship = value;
       }
+    }
+
+    public bool advancedSpaceStation()
+    {
+      return true;
+    }
+
+    public bool evasion()
+    {
+      return true;
+    }
+
+    public string highTech()
+    {
+      return "Advanced Communication";
     }
   }
 }
