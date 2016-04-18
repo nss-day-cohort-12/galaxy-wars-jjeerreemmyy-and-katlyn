316fe0e3d1ee6c39a7cf49296ee9be0d0c6bd0e7
Jeremy C. Swain <jeremycswain@gmail.com>
2016-04-18 09:57:56 -0600

    basic fight logic is implemented for two species.

--- GalaxyWars/GalaxyWars/BlackHole.cs
+++ GalaxyWars/GalaxyWars/BlackHole.cs
@@ -12,9 +12,36 @@ namespace GalaxyWars
     static void Main(string[] args)
     {
       MurkBlood murky = new MurkBlood();
-      murky.name = "Bobby";
+      murky.name = "Murky";
+      murky.planetName = "Krypton";
 
-      Console.WriteLine(murky.name);
+      Aldor aldy = new Aldor();
+      aldy.name = "Aldy";
+      aldy.planetName = "Krypton";
+
+      int y = 1;
+      double currMurkPop = murky.startingPopulation;
+      double currAldyPop = aldy.Population;
+      while (y < 25)
+      {
+        double religiousBonus = currMurkPop * .01;
+        currMurkPop = currMurkPop - 20000;
+        currMurkPop = currMurkPop - 2500;
+        currMurkPop = currMurkPop - religiousBonus;
+
+        currAldyPop = currAldyPop - 20000;
+        currAldyPop = currAldyPop - 10000;
+        currAldyPop = currAldyPop + religiousBonus;
+
+        
+
+        y++;
+        Console.WriteLine("The current population of Aldor is " + currAldyPop.ToString("#") + ".");
+        Console.WriteLine("The current population of MurkBlood is " + currMurkPop.ToString("#") + ".");
+      }
+
+
+      Console.WriteLine(murky.name + " is from " + murky.planetName + " and his people have been reduced to a population of " + currMurkPop.ToString("#"));
       Console.ReadLine();
     }
   }
